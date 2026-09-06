124af180b81345b2ff70219a85dd816c46034590
sudharsan <sudharsannadar1@gmail.com>
2026-09-06 14:28:06 +0530

    Initialized the API key

--- app/main.py
+++ app/main.py
@@ -4,3 +4,12 @@ from pydantic import BaseModel
 from groq import Groq
 import os
 from .auth import create_jwt_token, verify_token
+
+app = FastAPI()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+class LoginData(BaseModel):
+    username: str
+    password: str
+class ChatRequest(BaseModel):
+    question: str
